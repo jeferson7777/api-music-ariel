48abc7870b79462aa8c3d7e03edbf5a8d030c484
Arielis <aetumasonis@gmail.com>
2020-10-26 08:28:30 +0100

    add sass

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getMusicData } from './getMusic.js';
 
 function App() {
+  const [music, setMusic] = useState([]);
+
+  const handleChange = (event) => {
+    const { value } = event.currentTarget;
+    setMusic(getMusicData);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(song);
+  };
+
   return (
     <>
-      <h1>App Music List</h1>
+      <h1>Music app</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="song">Song</label>
+        <input
+          type="search"
+          id="song"
+          placeholder="Search song"
+          name="song"
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
     </>
   );
 }
 
 export default App;
+
+
